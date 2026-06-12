d824f7b512f3720bda2fc76612c6293ffdf92d94
Ashan Salinda <ashansalinda5@gmail.com>
2026-06-12 23:17:31 +0530

    Fix: formatting issue

--- infra/addons.tf
+++ infra/addons.tf
@@ -38,11 +38,30 @@ resource "helm_release" "aws_load_balancer_controller" {
   namespace  = "kube-system"
   version    = "1.8.1"
 
-  set { name = "clusterName"; value = module.eks.cluster_name }
-  set { name = "serviceAccount.create"; value = "false" }
-  set { name = "serviceAccount.name"; value = "aws-load-balancer-controller" }
-  set { name = "region"; value = "ap-south-1" }
-  set { name = "vpcId"; value = module.networking.vpc_id }
+  set {
+    name = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name = "region"
+    value = "ap-south-1"
+  }
+
+  set {
+    name = "vpcId"
+    value = module.networking.vpc_id
+  }
 
   depends_on = [
     kubernetes_service_account.aws_load_balancer_controller,
@@ -78,9 +97,20 @@ resource "helm_release" "external_secrets" {
   create_namespace = true
   version    = "0.15.0"
 
-  set { name = "installCRDs"; value = "true" }
-  set { name = "serviceAccount.create"; value = "false" }
-  set { name = "serviceAccount.name"; value = "external-secrets-sa" }
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-secrets-sa"
+  }
 
   depends_on = [
     kubernetes_service_account.external_secrets_sa,
@@ -115,8 +145,15 @@ resource "helm_release" "keda" {
   create_namespace = true
   version    = "2.15.0"
 
-  set { name = "serviceAccount.create"; value = "false" }
-  set { name = "serviceAccount.name"; value = "keda-operator" }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "keda-operator"
+  }
 
   depends_on = [
     kubernetes_service_account.keda_operator,
